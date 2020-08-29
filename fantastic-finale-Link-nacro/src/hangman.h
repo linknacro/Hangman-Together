9e1537fc2f8914f2712a548b71aadfa220c83460
Link Lin <ziqil4@illinois.edu>
2020-08-29 00:42:32 -0500

    fixed some word bank problems

--- fantastic-finale-Link-nacro/src/hangman.h
+++ fantastic-finale-Link-nacro/src/hangman.h
@@ -11,7 +11,7 @@ class Hangman {
 		vector<std::string> word_base_{
 			"polymorphism", "cryptography", "cybersecurity", "hackathon",
 			"internship",   "javascript",  "debug", "dynamic", "easy", "programmer",
-			"hardware", "software", "memory", "imperative", "spaghetti", "penguin", "amazon"
+			"hardware", "software", "memory", "imperative", "spaghetti", "penguin", "amazon",
 			"television", "couch", "alcoholism", "nuclear", "engineer", "discovery"};
         vector<char> letters_;
 		std::string current_word_;
